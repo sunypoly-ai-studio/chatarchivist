a8d0582d776b7d6813226e2c1f6829062eb4b562
joocyNut <150487864+joocyNut@users.noreply.github.com>
2024-07-19 16:18:58 -0400

    7/19/2024
    ### Updates in ChatArchivist 0.89
    
    - **ChatArchivist has been ported to TiddlyWiki 5.3.6-prerelease**
        - This allows for the ai-tools plugin and collaboration with Jeremy.
    - **Temperature was added to OpenAI API call**
    - **MenuBar plugin disabled to make room for LeftBar plugin**
        - MenuBar buttons are still currently visible on the wiki until this is determined as a permanent change.
    - **OpenAI import Python script has been updated, still needs testing**
    
    ---
    
    ### To be added
    
    - A range widget to alter the temperature of API responses
    - User and Assistant toolbars on conversation prompts for ChatArchivist functions (select, annotate, etc.)
    
    ---
    
    ### Known bugs
    
    - Two story mode is covered by the sidebar on some machines.
        > ⚠️ **Two story mode may be removed in future versions due to concerns with stability.**

--- chatarchivist-v0.88/tiddlers/$___sq_Stories_CorePatch_ButtonDispatchMessage.js
+++
@@ -1,5 +0,0 @@
-var ButtonWidget = require("$:/core/modules/widgets/button.js").button;
-
-ButtonWidget.prototype.dispatchMessage = function(event) {
-	this.dispatchEvent({type: this.message, param: this.param, tiddlerTitle: this.getVariable("currentTiddler"), event: event, navigateFromNode: this});
-};
